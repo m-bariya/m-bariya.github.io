378613fe503e429c669cf2bff779c00d84e61d0e
m-bariya <mohini@berkeley.edu>
2025-04-29 17:36:01 +0000

    Deploying to gh-pages from @ m-bariya/m-bariya.github.io@ac4dd23a8c9608ee8511b46eb87cc78dff6e2e61 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -400,7 +400,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{
+          section: "News",},{id: "projects-scientific-illustration",
+          title: 'Scientific Illustration',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/scientific_illustration/";
+            },},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
